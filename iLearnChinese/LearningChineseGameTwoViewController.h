9682b371fec9d92b7942ee520005d904f416cbad
Fries <Fries@Friess-Mac.local>
2012-03-29 00:40:43 +0800

    Draw chinese characters game added. The game need to be linked to the database to be fully operational. Moreover, a check on the keyboard activated need to be done.

--- iLearnChinese/LearningChineseGameTwoViewController.h
+++ iLearnChinese/LearningChineseGameTwoViewController.h
@@ -9,5 +9,36 @@
 #import <UIKit/UIKit.h>
 
 @interface LearningChineseGameTwoViewController : UIViewController
+<UITextFieldDelegate>
+{
+    NSInteger actualScore;
+    NSInteger nbQuestions;
+    NSInteger goodAnswer;
+    
+    NSInteger time;
+    NSInteger restTime;
+    BOOL isResting;
+    NSTimer *timer;
+    
+    NSInteger rightAnswer;
+    //NSString *rightAnswer;
+    
+    NSArray *myDB;
+    NSInteger countDB;
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *purpose;
+@property (weak, nonatomic) IBOutlet UILabel *nbQuestion;
+@property (weak, nonatomic) IBOutlet UILabel *question;
+@property (weak, nonatomic) IBOutlet UILabel *myScore;
+@property (weak, nonatomic) IBOutlet UILabel *timeLeft;
+@property (weak, nonatomic) IBOutlet UITextField *answerField;
+
+- (IBAction)valideAnswer:(id)sender;
+
+- (void) loadDB;
+- (void) loadQuestion;
+- (void) updateScore;
+- (void) countDown;
 
 @end
