ec739c08597dc56369719f9e1af0e95d119d478b
Jean-Laurent <jean-laurent.louis@epitech.eu>
2012-03-28 14:11:00 +0800

    ADD viewController for the database

---
+++ iLearnChinese/LearningChineseDatabaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  LearningChineseDatabaseViewController.h
+//  iLearnChinese
+//
+//  Created by jean-laurent louis on 3/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LearningChineseDatabaseViewController : UITableViewController
+
+@end
