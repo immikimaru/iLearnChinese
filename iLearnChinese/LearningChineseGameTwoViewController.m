339388963b34467b7fe1b3098f4863739563a92a
Fries <Fries@Friess-Mac.local>
2012-03-30 12:27:01 +0800

    Fix a problem because JL is stupid. There was some more few line to uncomment and some more to delete, moron. Add a few extra time to the second game.

--- iLearnChinese/LearningChineseGameTwoViewController.m
+++ iLearnChinese/LearningChineseGameTwoViewController.m
@@ -70,7 +70,7 @@
 {
     // Reset value for a new game
     actualScore = 0;
-    time = 60;
+    time = 300;
     nbQuestions = 0;
     goodAnswer = 0;
     nbQuestion.text = [[NSString alloc] initWithFormat:@"Question %i :", nbQuestions];
@@ -88,8 +88,7 @@
     rightAnswer = arc4random() % countDB;
     nbQuestions++;
     nbQuestion.text = [[NSString alloc] initWithFormat:@"Question %i:", nbQuestions];
-    //question.text = [[NSString alloc] initWithFormat:[[myDB objectAtIndex:rightAnswer] valueForKey:@"pinyin"]];
-    question.text = [[NSString alloc] initWithFormat:[myDB objectAtIndex:(rightAnswer * 2 + 1)]];
+    question.text = [[NSString alloc] initWithFormat:[[myDB objectAtIndex:rightAnswer] valueForKey:@"pinyin"]];
     // Set purpose of the game
     purpose.text = @"Draw the chinese character !";
     [answerField becomeFirstResponder];
@@ -100,57 +99,17 @@
 
 - (void) loadDB
 {
-    // Uncomment when real database is implemented
      NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
      NSEntityDescription *entity = [NSEntityDescription entityForName:@"Word" inManagedObjectContext:self.managedObjectContext];
      [fetchRequest setEntity:entity];
      NSError *error;
      myDB = [self.managedObjectContext executeFetchRequest:fetchRequest error:&error];
      countDB = myDB.count;
-#warning I'm fabulous
-    // Data are loaded. Something must be wrong in the algorithm didn't check further. Need some sleep :)
     for (Word *word in myDB)
     {
         NSLog(@"[JL DEBUG %s]\nE: %@\nC: %@", __func__, word.english, word.chinese);
     }
     NSLog(@"[JL DEBUG %s] LOAD DB : %i", __func__, countDB);
-    /*
-    // Virtual DB for prototype
-   NSArray *quizArray = [[NSArray alloc] initWithObjects:
-                          @"护照",      @"Passport",
-                          @"入境卡",    @"Entry card",
-                          @"机票",      @"Plane ticket",
-                          @"登记卡",    @"Boarding pass",
-                          @"学习",      @"Study",
-                          @"工作",      @"Work",
-                          @"经商",      @"Do business",
-                          @"目的",      @"Purpose",
-                          @"陆游",      @"Travel",
-                          @"来",       @"Come",
-                          @"中国",      @"China",
-                          @"箱子",      @"Suitcase",
-                          @"背包",      @"Backpack",
-                          @"行李",      @"Luggage",
-                          @"袋子",      @"Bag",
-                          @"出口",      @"Exit",
-                          @"入口",      @"Entrance",
-                          @"时间表",    @"Timetable",
-                          @"抵达",      @"Arrival",
-                          @"出发",      @"Departure",
-                          @"需要",      @"Need",
-                          @"相机",      @"Camera",
-                          @"书",       @"Books",
-                          @"光盘",      @"CD",
-                          @"申报",      @"Declaration",
-                          @"动物",      @"Dog",
-                          @"可以",      @"Can",
-                          @"带入",      @"Bring",
-                          @"水果",       @"Fruits",
-                          @"肉制品",     @"Meat products",
-                          @"植物",      @"Plants",
-                          nil];
-    countDB = quizArray.count / 2;
-	myDB = quizArray;*/
 }
 
 - (void)countDown
@@ -212,8 +171,7 @@
 }
 
 - (IBAction)valideAnswer:(id)sender {
-    //if([answerField.text isEqual:[[myDB objectAtIndex:rightAnswer] valueForKey:@"chinese"]])
-    if ([answerField.text isEqual:[myDB objectAtIndex:rightAnswer * 2]])
+    if([answerField.text isEqual:[[myDB objectAtIndex:rightAnswer] valueForKey:@"chinese"]])
     {
         purpose.text = @"很好！You scored.";
         goodAnswer++;
@@ -222,8 +180,7 @@
     else
     {
         purpose.text = @"That's wrong, too bad...";
-        //question.text = [[NSString alloc] initWithFormat:@"The right answer was %@", [[myDB objectAtIndex:rightAnswer] valueForKey:@"chinese"]];
-        question.text = [[NSString alloc] initWithFormat:@"%@ != %@", answerField.text, [myDB objectAtIndex:rightAnswer * 2]];
+        question.text = [[NSString alloc] initWithFormat:@"The right answer was %@", [[myDB objectAtIndex:rightAnswer] valueForKey:@"chinese"]];
         actualScore -= 2;
     }
     answerField.enabled = NO;
