d2f5462a5952167b3e5de0537041bcc1b9a1f5bb
Jean-Laurent <jean-laurent.louis@epitech.eu>
2012-03-30 17:09:25 +0800

    Adding pictures for the main view

---
+++ iLearnChinese/LearningChineseHighScoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  LearningChineseHighScoreViewController.h
+//  iLearnChinese
+//
+//  Created by jean-laurent louis on 3/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LearningChineseHighScoreViewController : UITableViewController
+
+@end
